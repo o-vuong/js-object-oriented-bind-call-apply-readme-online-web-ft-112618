514548d5d41c494458134067441360ff4e36c1c4
o-vuong <oscarvuong@gmail.com>
2019-03-18 21:16:01 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,56 @@
 // use this to copy code snippets or use your browser's console
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+
+greet();
+
+let person = {
+    name: `Bob`,
+    greet: greet
+};
+
+person.greet();
+
+
+
+function greet() {
+    console.log(`my name is ${this.name}, hi!`);
+}
+
+let sally = { name: `Sally` };
+
+greet.call(sally);
+
+greet.apply(sally);
+
+
+
+function greet(customerOne, customerTwo) {
+    console.log(`Hi ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
+
+let sally = { name: `Sally` };
+function greet(customerOne, customerTwo) {
+    console.log(`Hi, ${customerOne} and ${customerTwo}, my name is ${this.name}!`);
+}
+
+greet.call(sally, `Terry`, `George`);
+
+
+greet.call(sally);
+
+greet.apply(sally, [`Terry`, `George`]);
+
+let sally = { name: 'Sally' };
+
+function greet(customer) {
+	console.log(`Hi ${customer}, my name is ${this.name}!`);
+}
+
+let newGreet = greet.bind(sally);
+
+newGreet('Bob');
+
+
+greet('Bob');
